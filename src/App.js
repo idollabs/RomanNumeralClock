6ecd534170541161e13f3b4c183b9b9cf5793da7
Andrew Dauphinais <idollabschicago@gmail.com>
2020-12-23 17:58:23 -0600

    Completed main code. Untested with two digit hour values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Minute from './minute';
 
-function App() {
+const App = () => {
+  const [dt, setDt] = useState(new Date().toLocaleString());
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    let secTimer = setInterval(() => {
+      setDt(new Date().toLocaleString());
+    }, 1000);
+
+    return () => clearInterval(secTimer);
+  }, []);
+
+  let hourString = dt.slice(11, -9);
+  let hourNum = parseInt(hourString);
+  let minuteString = dt.slice(14 + value, -6);
+  let minuteNum = parseInt(minuteString);
+  let secondString = dt.slice(17 + value, -3);
+  let secondNum = parseInt(secondString);
+
+  const colan = (time) => {
+    if (time !== 0) {
+      return ':';
+    }
+  };
+
+  const adjustSplice = () => {
+    if (hourNum >= 10) {
+      setValue(1);
+    } else {
+      setValue(0);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>TEMPUS</h1>
+      <div className='clock'>
+        {Minute[hourNum]} <span id='colan'>:</span> {Minute[minuteNum]}{' '}
+      </div>
+      <div id='seconds'>
+        {colan(secondNum)} {Minute[secondNum]}
+      </div>
+    </>
   );
-}
+};
 
 export default App;

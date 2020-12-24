4b6b1a678ebad132ae3e8431a15da669a1346c2d
Andrew Dauphinais <idollabschicago@gmail.com>
2020-12-24 16:50:51 -0600

    made military time toggle and added styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Minute from './minute';
 const App = () => {
   const [dt, setDt] = useState(new Date().toLocaleString());
   const [value, setValue] = useState(0);
+  const [military, setMilitary] = useState(0);
+  const [isMilitary, setIsMilitary] = useState(true);
+  const [hourButtonValue, setHourButtonValue] = useState('');
+  const [showAmPm, setShowAmPm] = useState('');
+
   useEffect(() => {
     let secTimer = setInterval(() => {
       setDt(new Date().toLocaleString());
@@ -12,12 +17,38 @@ const App = () => {
     return () => clearInterval(secTimer);
   }, []);
 
+  useEffect(() => {
+    let secTimer = setInterval(() => {
+      if (hourNum >= 10) {
+        setValue(1);
+      } else {
+        setValue(0);
+      }
+    }, 1000);
+
+    return () => clearInterval(secTimer);
+  }, []);
+
+  useEffect(() => {
+    if (isMilitary && timeOfDay === 'PM' && hourNum !== 12) {
+      setMilitary(12);
+      setHourButtonValue('XII Horam Horologium');
+      setShowAmPm('');
+    } else {
+      setMilitary(0);
+      setHourButtonValue('XXIV Horam Horologium');
+      setShowAmPm(timeOfDay);
+    }
+  }, [isMilitary]);
+
   let hourString = dt.slice(11, -9);
-  let hourNum = parseInt(hourString);
+  let hourConst = parseInt(hourString);
+  let hourNum = parseInt(hourString) + military;
   let minuteString = dt.slice(14 + value, -6);
   let minuteNum = parseInt(minuteString);
   let secondString = dt.slice(17 + value, -3);
   let secondNum = parseInt(secondString);
+  let timeOfDay = dt.slice(20 + value);
 
   const colan = (time) => {
     if (time !== 0) {
@@ -25,24 +56,29 @@ const App = () => {
     }
   };
 
-  const adjustSplice = () => {
-    if (hourNum >= 10) {
-      setValue(1);
+  const makeMilitary = () => {
+    if (!isMilitary) {
+      setIsMilitary(true);
     } else {
-      setValue(0);
+      setIsMilitary(false);
     }
   };
 
   return (
-    <>
+    <main>
       <h1>TEMPUS</h1>
+      <button id='militaryBtn' onClick={makeMilitary}>
+        {hourButtonValue}
+      </button>
+
       <div className='clock'>
-        {Minute[hourNum]} <span id='colan'>:</span> {Minute[minuteNum]}{' '}
+        {Minute[hourNum]} <span id='colan'>:</span> {Minute[minuteNum]}
       </div>
       <div id='seconds'>
-        {colan(secondNum)} {Minute[secondNum]}
+        {/* {colan(secondNum)}  */}: {Minute[secondNum]}
       </div>
-    </>
+      <article id='timeOfDay'>{showAmPm}</article>
+    </main>
   );
 };
 

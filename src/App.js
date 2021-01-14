38f8e8f85f5e723daf77e833cde467caed738200
Andrew Dauphinais <idollabschicago@gmail.com>
2021-01-14 15:20:02 -0600

    Massive changes to styling. Added new columns and curtain

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Clock from './Clock';
 const App = () => {
   return (
     <main>
+      {/* <a href='https://pngtree.com/so/famous-buildings'>
+        famous buildings png from pngtree.com
+      </a> */}
       <Clock />
     </main>
   );
--- src/Clock.js
+++ src/Clock.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Minute from './minute';
+import pillarMiddle from './images/romanpillarelementsmiddle.png';
+import pillarTop from './images/romanpillarelementstop.png';
+import pillarBottom from './images/romanpillarelementsbottom.png';
 
 const Clock = () => {
   const [dt, setDt] = useState(new Date());
@@ -44,21 +47,39 @@ const Clock = () => {
 
   return (
     <>
-      <div className='title-and-button'>
-        <h1>TEMPVS</h1>
-
-        <button id='militaryBtn' onClick={makeMilitary}>
-          {buttonValue}
-        </button>
-      </div>
       <div className='appHousing'>
-        <div className='clockHousing'>
-          <div className='clock'>
-            {Minute[hours12(dt.getHours())]} <span id='colan'>: </span>
-            {Minute[dt.getMinutes()]}
+        <div id='topper'>
+          <img id='leftPillar' src={pillarTop} alt='' />
+          <div className='spacer'>
+            <div className='title-and-button'>
+              <h1>TEMPVS</h1>
+
+              <button id='militaryBtn' onClick={makeMilitary}>
+                {buttonValue}
+              </button>
+              <br />
+            </div>
           </div>
-          <div id='seconds'>: {Minute[dt.getSeconds()]}</div>
-          <article id='timeOfDay'>{amPmHandler(dt.getHours())}</article>
+          <img id='rightPillar' src={pillarTop} alt='' />
+        </div>
+
+        <div className='clockHousing'>
+          <img className='leftPillar' src={pillarMiddle} alt='' />
+          <div className='clockInnerHousing'>
+            <div className='clock'>
+              {Minute[hours12(dt.getHours())]} <span id='colan'>: </span>
+              {Minute[dt.getMinutes()]}
+            </div>
+            <div id='seconds'>: {Minute[dt.getSeconds()]}</div>
+            <article id='timeOfDay'>{amPmHandler(dt.getHours())}</article>
+          </div>
+          <img className='rightPillar' src={pillarMiddle} alt='' />
+        </div>
+
+        <div id='bottom'>
+          <img id='leftPillar' src={pillarBottom} alt='' />
+          <div className='spacer'></div>
+          <img id='rightPillar' src={pillarBottom} alt='' />
         </div>
       </div>
     </>
